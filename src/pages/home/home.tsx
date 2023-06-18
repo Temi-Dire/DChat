9fc11b23efd1e5f5e73a9b5d8c300b309f334ca6
Temi-Dire <temidireowoeye@gmail.com>
2023-06-18 07:10:50 +0100

    created home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,9 @@
+const home = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center font-extrabold text-4xl">
+        D-CHAT
+    </div>
+  )
+}
+
+export default home
