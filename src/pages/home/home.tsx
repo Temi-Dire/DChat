49a1dca2206636a766dab75f794b4fdc3249f5fa
Temi-Dire <temidireowoeye@gmail.com>
2023-06-19 23:19:35 +0100

    fix: added chats

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,8 @@
 import SearchIcon from '@mui/icons-material/Search';
+// import contact from "../../assets/contacts"
 const home = () => {
   return (
-    <div className='h-screen bg-[#ece0f7] pt-2 flex flex-col'>
+    <div className='h-screen bg-[#ece0f7] pt-4 flex flex-col'>
         <nav className='flex justify-center'>
             <div className='w-4/5 bg-white flex justify-between px-6 py-3 rounded-3xl'>
                 <input className='w-full outline-none' type="text" />
@@ -13,8 +14,79 @@ const home = () => {
                 CHATS
             </h1>
         </div>
-        <div className='w-full h-[200px] bg-white rounded-t-3xl flex-auto'>
-
+        <div className='w-full h-[200px] bg-white rounded-t-3xl flex-auto p-5'>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div className=''>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
+            <div className='flex justify-between mb-6 items-center'>
+                <div className='bg-[#ece0f7] w-16 h-16 rounded-full '></div>
+                <div>
+                    <h1 className='font-bold'>Owoeye Temidire</h1>
+                    <p className='text-xs'>Hey What's up, Long time no see!</p>
+                </div>
+                <div>3 min ago</div>
+            </div>
         </div>
     </div>
   )
