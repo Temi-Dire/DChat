842913d89e0394551c2b364046919d0efab4471b
Temi-Dire <temidireowoeye@gmail.com>
2023-06-19 23:19:07 +0100

    created data for contact

---
+++ src/assets/contacts.tsx
@@ -0,0 +1,27 @@
+const contacts = [
+  {
+    name: "Owoeye Temidire",
+    message: "Hey Bro, How are you doing",
+    time: "3 mins ago",
+  },
+  {
+    name: "James Bond",
+    message: "How are you?",
+    time: "3 mins ago",
+  },
+  {
+    name: "John Wick",
+    message: "Are you okay?",
+    time: "3 mins ago",
+  },
+  {
+    name: "Emmanuel Jonathan",
+    message: "Where are you brother?",
+    time: "3 mins ago",
+  },
+  {
+    name: "Sam Smith",
+    message: "Nope, I have not yet done it",
+    time: "3 mins ago",
+  },
+];
