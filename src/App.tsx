48378fa89dae6d1c292f6e8884c22cc4f9e9bde6
Temi-Dire <temidireowoeye@gmail.com>
2023-06-19 13:26:54 +0100

    initialized home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Signup from "./pages/auth/signup"
 import Signin from "./pages/auth/signin"
 import Home from "./pages/home/home"
 import Error from "./pages/error/error"
+import Loadhome from "./pages/home/loadhome"
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -14,10 +15,11 @@ function App() {
     <Router>
       <>
         <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Loadhome />} />
         <Route path="*" element={<Error />} />
         <Route path="/auth/signup" element={<Signup />} />
         <Route path="/auth/signin" element={<Signin />} />
+        <Route path="/home" element={<Home />} />
         </Routes>
       </>
     </Router>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,21 @@
+import SearchIcon from '@mui/icons-material/Search';
 const home = () => {
   return (
-    <div className="flex w-full h-screen justify-center items-center font-extrabold text-4xl">
-        D-CHAT
+    <div className='h-screen bg-[#ece0f7] pt-2 flex flex-col'>
+        <nav className='flex justify-center'>
+            <div className='w-4/5 bg-white flex justify-between px-6 py-3 rounded-3xl'>
+                <input className='w-full outline-none' type="text" />
+                <SearchIcon className='ml-4' />
+            </div>
+        </nav>
+        <div>
+            <h1 className='mt-8 mb-4 ml-4 font-bold text-xl'>
+                CHATS
+            </h1>
+        </div>
+        <div className='w-full h-[200px] bg-white rounded-t-3xl flex-auto'>
+
+        </div>
     </div>
   )
 }
